b15fdedb2905a850447f6d46ecdd4edfd707b4e5
Lokusok <lokusokdrobk@gmail.com>
2024-11-04 19:23:49 +0300

    change config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://lokusok.github.io',
+    base: 'lokusok-s',
+});
